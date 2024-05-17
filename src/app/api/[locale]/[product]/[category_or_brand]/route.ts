93c129c6347eb1df96cc0e07f343032a46ddca7c
lucmanm <greenjamez22@gmail.com>
2024-05-17 22:46:14 +0300

    applied route query for category and brand

---
+++ src/app/api/[locale]/[product]/[category_or_brand]/route.ts
@@ -0,0 +1,46 @@
+import { prisma } from "@/lib/db";
+import { error } from "console";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest, { params }: { params: { locale: string, category_or_brand: string } }) {
+
+    const { locale, category_or_brand } = params
+    const searchParams = request.nextUrl.searchParams
+    const querySkip = searchParams.get("skip")
+    const queryTake = searchParams.get("take")
+
+
+    try {
+        // CONTINUE:  check all language error handling
+        const checklanguage = await prisma.language.findUnique({
+            where: { languageCode: locale }
+        })
+        if (!checklanguage) {
+            console.log("LANGUAGE_ERROR_UNAVAILABLE", error);
+        }
+        
+        const result = await prisma.product.findMany({
+            where: {
+                AND: [
+                    {
+                        categoryName: {
+                            contains: category_or_brand,
+                            mode: "insensitive"
+                        }
+                    }
+                ]
+            },
+            skip: Number(querySkip),
+            take: Number(queryTake),
+            orderBy:{
+                updatedAt: "asc"
+            }
+        })
+        return NextResponse.json({ result }, { status: 200 })
+
+    } catch (error) {
+        console.log("ERROR_CATEGORY_OR_BRAND", error);
+
+    }
+}
+
--- src/app/api/[locale]/[product]/route.ts
+++ src/app/api/[locale]/[product]/route.ts
@@ -1,34 +1,27 @@
 import { prisma } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, { params }: { params: { locale: string, product: string } }) {
+export async function GET(request: Request, { params }: { params: { locale: string, product: string } }) {
     const { product, locale } = params
-    const productDescription = decodeURIComponent(product.replace(/\\s+/g, ""))
+    const productmodel = decodeURIComponent(product.replace(/\\s+/g, ""))
+
     try {
-        const changeLanguage = await prisma.language.findFirst({
-            where: {
-                languageCode: locale
+        
+            const product = await prisma.product.findUnique({
+                where: {
+                    model: productmodel,
+                }
+            })
+
+            if (product) {
+
+                return NextResponse.json({ product }, { status: 200 })
             }
-        })
-        if (!changeLanguage) {
-            console.log("ERROR_API_GET_PRODUCT");
-            return NextResponse.json({ "Error": "Bad Request" }, { status: 500 })
-        }
-
-        const result = await prisma.product.findFirst({
-            where: {
-                shortDescriptionEn: productDescription
-            },
-        })
-
-        if (!result) {
-            console.log("ERROR_API_GET_PRODUCT");
-            return NextResponse.json({ "Error": "Bad Request" }, { status: 500 })
-        }
-        return NextResponse.json(result, { status: 200 })
-
-
+            else {
+                return NextResponse.json({ "Error": "Page not Found" }, { status: 500 } )
+            }
     } catch (error) {
-        console.log("ERROR_API_GET_PRODUCT", error);
+        console.log("ERROR_API_GET_PRODUCT_AND_BRAND", error);
     }
+
 }
